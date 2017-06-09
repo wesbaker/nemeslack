bc03f636a594b3e36f706b0cc29837f54fcae87b
Wes Baker <wes@wesbaker.com>
2017-06-09 14:21:12 -0400

    Create play wrapper, automated tests, add Travis and Coveralls
    Fixes #2
    Fixes #3
    Fixes #6
    Fixes #7

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,19 @@
 module.exports = {
-    "env": {
-        "es6": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "sourceType": "module"
-    },
-    "rules": {
-        "indent": [
-            "error",
-            2
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "double"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ],
-        "no-console": [
-          0
-        ]
-    }
+  env: {
+    es6: true,
+    node: true,
+    "jest/globals": true
+  },
+  plugins: ["jest"],
+  extends: ["eslint:recommended", "plugin:jest/recommended"],
+  parserOptions: {
+    sourceType: "module"
+  },
+  rules: {
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+    "no-console": [0]
+  }
 };
