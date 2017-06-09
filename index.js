4815d06641d3b527834a4e45de191d43192c64dc
Wes Baker <wes@wesbaker.com>
2017-06-09 14:21:12 -0400

    Create play wrapper, automated tests, add Travis and Coveralls
    Fixes #2
    Fixes #3
    Fixes #6
    Fixes #7

--- index.js
+++ index.js
@@ -2,56 +2,10 @@ require("dotenv").config();
 
 const IncomingWebhook = require("@slack/client").IncomingWebhook;
 const axios = require("axios");
-const xmldoc = require("xmldoc");
 const moment = require("moment");
-
 const url = process.env.SLACK_WEBHOOK_URL || "";
 const webhook = new IncomingWebhook(url);
-
-const getUrl = play =>
-  `https://nemestats.com/PlayedGame/Details/${play.playedGameId}`;
-
-const createPlayers = players => {
-  // Check to make sure _someone_ scored points
-  const points = players.some(
-    player => player.pointsScored !== null && player.pointsScored !== 0
-  );
-  const names = players.map(player => {
-    const prefix = player.gameRank === 1 ? ":trophy: " : "";
-    const name = player.playerName.split(" ")[0];
-    const score = points ? ` (${player.pointsScored})` : "";
-    return prefix + name + score;
-  });
-
-  return { title: "Players", value: names.join(", "), short: true };
-};
-
-const getThumbnail = bggId => {
-  return axios
-    .get("https://www.boardgamegeek.com/xmlapi2/thing", {
-      params: { id: bggId }
-    })
-    .then(response => {
-      const document = new xmldoc.XmlDocument(response.data);
-      return document.valueWithPath("item.thumbnail");
-    })
-    .catch(err => console.error(err));
-};
-
-const createAttachment = (play, thumbUrl) => {
-  const date = moment(play.datePlayed).format("dddd, MMMM Do YYYY");
-  return [
-    {
-      fallback: "Required plain-text summary of the attachment.",
-      color: "#354458",
-      title: play.gameDefinitionName,
-      title_link: getUrl(play),
-      text: `Played on ${date}`,
-      fields: [createPlayers(play.playerGameResults)],
-      thumb_url: thumbUrl
-    }
-  ];
-};
+const getAttachments = require("./lib/Play").getAttachments;
 
 axios
   .get("https://nemestats.com/api/v2/PlayedGames/", {
@@ -62,9 +16,9 @@ axios
     }
   })
   .then(({ data: { playedGames } }) => {
-    playedGames.forEach(play => {
-      getThumbnail(play.boardGameGeekGameDefinitionId).then(thumbUrl => {
-        webhook.send({ attachments: createAttachment(play, thumbUrl) });
+    playedGames.forEach(playData => {
+      getAttachments(playData).then(attachments => {
+        webhook.send({ attachments });
       });
     });
   })
