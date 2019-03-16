741bc78d53ff2351eaaec47349cc804c75696fb4
Wes Baker <wes@wesbaker.com>
2019-03-16 09:12:24 -0400

    Missed some moment functions, more async issues

--- check.js
+++ check.js
@@ -15,8 +15,11 @@ if (testRun) {
   if (typeof axios !== "function") {
     console.error("axios was not loaded.");
   }
-  if (typeof moment !== "function") {
-    console.error("moment was not loaded.");
+  if (typeof format !== "function") {
+    console.error("format was not loaded.");
+  }
+  if (typeof subDays !== "function") {
+    console.error("subDays was not loaded.");
   }
   console.log("Debug: Nemeslack would post now.");
   process.exit(0);
@@ -28,8 +31,8 @@ Raven.config(process.env.SENTRY_DSN);
 const url = process.env.SLACK_WEBHOOK_URL;
 const webhook = new IncomingWebhook(url);
 
-module.exports = (req, res) => {
-  axios
+module.exports = async (req, res) => {
+  await axios
     .get("https://nemestats.com/api/v2/PlayedGames/", {
       params: {
         gamingGroupId: process.env.GAMING_GROUP_ID,
@@ -38,14 +41,17 @@ module.exports = (req, res) => {
       }
     })
     .then(({ data: { playedGames } }) => {
-      playedGames.forEach(playData => {
-        getAttachments(playData).then(attachments => {
+      const promises = playedGames.map(playData => {
+        return getAttachments(playData).then(attachments => {
           webhook.send({ attachments }, err => {
             if (err) Raven.captureException(err);
-            res.end(`${playedGames.length} plays sent to Slack.`);
           });
         });
       });
+
+      return Promise.all(promises).then(() =>
+        res.end(`${playedGames.length} plays sent to Slack.`)
+      );
     })
     .catch(err => {
       Raven.captureException(err);
