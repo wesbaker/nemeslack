9639c7a5d587a86fa0e2e0090392c3b440382ba4
wesbaker <wes@wesbaker.com>
2021-01-18 16:54:09 -0500

    Remove Jest coverage config

--- jest.config.ts
+++ jest.config.ts
@@ -2,14 +2,6 @@ import type { Config } from "@jest/types";
 
 const config: Config.InitialOptions = {
   coverageDirectory: "./coverage/",
-  coverageThreshold: {
-    global: {
-      branches: 100,
-      functions: 100,
-      lines: 100,
-      statements: 100,
-    },
-  },
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   testEnvironment: "node",
   testPathIgnorePatterns: ["/node_modules/", "jest.config.ts"],
